aed365bdeb62c9b70fcc46841b00cbb4f4b3e3d7
Dellius Alexander <45150032+dellius-alexander@users.noreply.github.com>
2022-09-19 15:48:11 -0400

    initial commit

--- helloworld/src/main/java/com/example/helloworld/HelloActivity.kt
+++ helloworld/src/main/java/com/example/helloworld/HelloActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HelloActivity : AppCompatActivity() {
+    /**
+     * we override the default implementation, and call super.onCreate to initialize an
+     * activity using a previous savedInstanceState and following that we map the activity
+     * to a view.
+     * @param savedInstanceState a previous savedInstanceState
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello)
     }
+
 }
