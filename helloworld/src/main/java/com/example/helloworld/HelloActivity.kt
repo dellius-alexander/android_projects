a2da12ad639b036b8c9f454a9e4a6e5ed1c34913
Dellius Alexander <45150032+dellius-alexander@users.noreply.github.com>
2022-09-20 00:50:18 -0400

    updated helloworld app to use jetpack

--- helloworld/src/main/java/com/example/helloworld/HelloActivity.kt
+++ helloworld/src/main/java/com/example/helloworld/HelloActivity.kt
@@ -1,12 +1,25 @@
 package com.example.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.content.Intent
-import kotlinx.android.synthetic.main.activity_hello.*
-import kotlinx.android.synthetic.main.activity_result.*
+import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.widget.Text
+import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.helloworld.databinding.ActivityHelloBinding
+import com.example.helloworld.databinding.ActivityResultBinding
+
+//import android.content.Intent
+//import android.widget.TextView
+//import kotlinx.android.synthetic.main.activity_hello.*
+//import kotlinx.android.synthetic.main.activity_result.*
 
 class HelloActivity : AppCompatActivity() {
+
+    private lateinit var resultBinding: ActivityResultBinding
+    private lateinit var helloBinding: ActivityHelloBinding
     /**
      * we override the default implementation, and call super.onCreate to initialize an
      * activity using a previous savedInstanceState and following that we map the activity
@@ -14,25 +27,44 @@ class HelloActivity : AppCompatActivity() {
      * @param savedInstanceState a previous savedInstanceState
      */
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)  // load from previously saved state
-        setContentView(R.layout.activity_hello)
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_hello)
 
-        /**
-         * we extract the activity from the submit button activity.
-         * We initialize an Intent object that points to the component to handle the
-         * intent, which in out case is the Results class. We then capture the input field
-         * get_email_address and assign the input to the intent which points to the
-         * Results activity.
-         */
+        // create instance of the ActivityMainBinding,
+        // as we have only one layout activity_main.xml
+        helloBinding = ActivityHelloBinding.inflate(layoutInflater)
+        resultBinding = ActivityResultBinding.inflate(layoutInflater)
+
+        // binding.root returns the root layout,
+        // which is activity_main.xml file itself
+        setContentView(helloBinding.root)
+
+//        // using the binding variable we can access the layout
+//        // properties and perform the operations on them as usual
+//        helloBinding.submitButton.setOnClickListener {
+//            val str: String = helloBinding.getEmailAddress.text.toString()
+//            if (str.isNotEmpty()) {
+//                resultBinding.resultText.text = str
+//
+//            } else {
+//                error("Something went wrong with key binding...")
+//            }
+//        }
+
+//        resultBinding = ActivityResultBinding.inflate(layoutInflater)
+//        helloBinding = ActivityHelloBinding.inflate(layoutInflater)
+        var submit_button = helloBinding.submitButton
+
+        var result_intent = Intent(this, Result::class.java)
+
         submit_button.setOnClickListener {
-            val result_intent = Intent(this, Result::class.java)
-            if (get_email_address.text != null){
-                val display_text = get_email_address.text
+            var email_text = helloBinding.getEmailAddress
+            if (email_text.text.isNotEmpty()){
+                val display_text = email_text.text
                 result_intent.putExtra("text", display_text.toString())
                 startActivity(result_intent)
                 println(display_text)
             }
-
         }
 
     }
--- helloworld/src/main/java/com/example/helloworld/Result.kt
+++ helloworld/src/main/java/com/example/helloworld/Result.kt
@@ -2,23 +2,45 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_hello.*
-import kotlinx.android.synthetic.main.activity_result.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.helloworld.databinding.ActivityHelloBinding
+//import com.example.helloworld.databinding.ActivityHelloBinding
+import com.example.helloworld.databinding.ActivityResultBinding
+
+//import kotlinx.android.synthetic.main.activity_hello.*
+//import kotlinx.android.synthetic.main.activity_result.*
 
 class Result : AppCompatActivity() {
+
+    private lateinit var resultBinding: ActivityResultBinding
+
     /**
      * we override the default implementation, and call super.onCreate to initialize an
      * activity using a previous savedInstanceState and following that we map the activity
      * to a view.
      * @param savedInstanceState a previous savedInstanceState
      */
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
 
-        // retrieve the value from a map
-        val text_to_be_displayed  = intent.getStringExtra("text")
-        // set the input message as the display text
-        display_text.text = text_to_be_displayed
+        // create instance of the ActivityMainBinding,
+        // as we have only one layout activity_hello.xml
+        resultBinding = ActivityResultBinding.inflate(layoutInflater)
+
+        // resultBinding.root returns the root layout,
+        // which is activity_hello.xml file itself
+        setContentView(resultBinding.root)
+
+        // create an intent to pass the text from activity_hello.xml
+        // to activity_result.xml TextView
+        val text_to_be_displayed = intent.getStringExtra("text")
+
+        // pass to both TextView in activity_result.xml
+        resultBinding.resultText.text = text_to_be_displayed
+        resultBinding.displayText.text = text_to_be_displayed
+        println(text_to_be_displayed)
+
     }
 }
